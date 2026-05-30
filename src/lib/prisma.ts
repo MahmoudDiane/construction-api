7eb4b0d79dfd36030d49c17da0d969baecb065ab
Mahmoud Diane <mahmouddianetech@gmail.com>
2026-05-30 13:01:07 +0100

    chore: setup prisma client and fix tsconfig

---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+import Prisma from "@prisma/client";
+
+const { PrismaClient } = Prisma;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: InstanceType<typeof PrismaClient> | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
